d5ebcf9d3f8599bc04ed753adb874e8906e7dc69
sanasuri101 <54214582+sanasuri101@users.noreply.github.com>
2024-08-26 18:26:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "sanasuri101.github.io",
+  base: "./sanasuri101.github.io",
   optimizeDeps: {
     // include: ['linked-dep'],
   },
